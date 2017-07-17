26f434470238c7398a5e26595d13e024dd251d92
diao7ni <dgangmagag@163.com>
2017-07-18 03:18:57 +0800

    new

---
+++ js/vueConent.js
@@ -0,0 +1,38 @@
+    var message = {
+        name:'姓名:梁锦麟',
+        job:'面试工作:web前端',
+        phone:'联系电话:18502045354',
+        mail:'联系邮箱:dgangmagag@163.com',
+        ex1:'2017/04 -2017/05   网站实例：diy购物网,网站链接:http://alinj.info/jjh/',
+        ex2:'1,责任描述：负责整个网站页面的静态实现（div+css布局，botstrap框架应用，html5+css3用户体验，浏览器兼容性。',
+        ex3:'2,工作描述：使用bootstrap框架和div+css实现总体布局。通过bootstrap框架制作，实现响应式布局，可以同时兼容手机，平板，PC端各大浏览器（ie8+）。',
+        ex4:'3,项目开发描述：1.除框架外均为原生代码。2.网站遵循W3C标准，兼容性好。3，代码符合HTML语义化。4，通过PS对网站相关图片进行修改。',
+        
+        major1:'1,熟练使用h5和css，能独立使用div+css进行页面布局。会使用js，编写简单js代码。会使用jq库，如会通过选择器选取元素并添加事件，动画等操作。正在通过《javascript权威指南》学习js和jq',
+        major2:'2,熟练使用bootstrap框架，会独立编写响应式。了解vue.js框架，并正在学习中',
+        jieshao:'',
+        
+        ex11:'2017/03 - 2017/04 项目名称：个人博客,链接：http://alinj.info/',
+        ex12:'工作描述：通过hexo+github制作个人博客',
+        
+        
+        ex21:'2017/7-2017/7 网站实例:个人简历,网站链接:当前页面',
+        ex22:'工作描述:通过vue.js框架,jq库制作，通过使用vue.js实现数据和布局分离。并使用jQuery，jQuery库的waypoints插件实现滚动监听，通过animate.css实现动画效果',
+        
+        page1:'以上两张图片分别为diy购物网pc端和手机端的样式，使用bootstrap框架，实现响应式布局，并使用bootstrap框架的visible和hidden属性为pc端和手机端设置不同的样式',
+        page2:'1,通过vue.js框架，view使用简洁的代码实现，数据通过js传入，分离视图和数据，修改数据不影响视图',
+        page22:'2,使用waypoint插件实现滚动监听。先获取当前元素，当滚动预设高度时触发事件，添加animated动画'
+        
+        
+    }
+
+    var vm = new Vue({
+        el:'#vbody',
+        data:message
+    })
+
+    
+
+
+
+
